cf19c0916ad282a602d444c026af8f08865dbf96
m0rtal <m0rtal@mail.ru>
2020-10-19 19:19:57 +0300

    second test

---
+++ src/test2/test.java
@@ -0,0 +1,12 @@
+//2. Повторить задание 1, но используя цикл ​for​
+
+
+package test2;
+
+public class test {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.print("JAVA ");
+        }
+    }
+}
