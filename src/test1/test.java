126ac3fd4bd868676c40930f37d38befc0366fab
m0rtal <m0rtal@mail.ru>
2020-10-19 19:14:27 +0300

    Initialcommit

---
+++ src/test1/test.java
@@ -0,0 +1,18 @@
+//Задачи:
+//        1. Вывести на экран слово ​“JAVA”​, в строку, чтобы оно повторилось 10 раз, используя
+//        цикл ​while​.
+//        Вывод в консоль должен быть таким:
+//        JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+
+package test1;
+
+public class test {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.print("JAVA ");
+            i++;
+        }
+
+    }
+}
